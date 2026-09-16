eed189328569f15d003780b282448ea58d309eab
Lawrence Abudetse <lawrenceabudetse75@gmail.com>
2026-09-16 13:10:53 +0000

    Update calvis.py

--- calvis.py
+++ calvis.py
@@ -26,7 +26,13 @@ class users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=True)
-
+    
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    date_sent = db.Column(db.DateTime, default=datetime.utcnow)
+    
     def __init__(self, name, email):
         self.name = name
         self.email = email
